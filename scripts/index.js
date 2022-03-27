a13f4b922ad70994af0aa6d4e61e9f977c130cca
Tin Tadić <tin.tadic46@gmail.com>
2022-03-27 12:11:05 +0200

    second homework

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+function showHint(str) {
+    if (str.length == 0) { 
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    } else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {			
+				let res = xmlhttp.responseText;
+				let arrRes = res.split("||");
+				//removes the last element, which is "||"
+				arrRes.pop();
+				
+				if(arrRes.length != 0) {
+					document.getElementById("txtHint").innerHTML = "<tr><th>RESULT</th><th>DETAILS</th><th>UPDATE</th><th>DELETE</th></tr>";
+					arrRes.forEach(item => {
+						document.getElementById("txtHint").innerHTML += "<tr><td>" + item + "<td><a href='#'>DETAILS</a></td><td><a href='#'>UPDATE</a></td><td><a href='#'>DELETE</a></td></tr>";
+					});
+				} else {
+					document.getElementById("emptyRes").innerText = "Ne podudara se nista sa tom pretragom.";
+				}
+				
+
+            }
+        }
+        xmlhttp.open("GET", "php/productGetter.php?searchFor=" + str, true);
+        xmlhttp.send();
+    }
+}
