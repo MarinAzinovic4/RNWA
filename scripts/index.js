8e4162ef98ae35175e35387cc09992fc991e9c5b
Tin Tadić <tin.tadic46@gmail.com>
2022-04-03 18:55:32 +0200

    implement ajax to table

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,23 @@
 function showHint(str) {
     if (str.length == 0) { 
-        document.getElementById("txtHint").innerHTML = "";
+        $("#txtHint").html("");
         return;
     } else {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {			
-				let res = xmlhttp.responseText;
-				let arrRes = res.split("||");
-				//removes the last element, which is "||"
-				arrRes.pop();
-				
-				if(arrRes.length != 0) {
-					document.getElementById("emptyRes").innerText = "";
-					document.getElementById("txtHint").innerHTML = "<tr><th>RESULT</th><th>DETAILS</th><th>UPDATE</th><th>DELETE</th></tr>";
-					arrRes.forEach(item => {
-						document.getElementById("txtHint").innerHTML += "<tr><td>" + item + "<td><a href='#'>DETAILS</a></td><td><a href='#'>UPDATE</a></td><td><a href='#'>DELETE</a></td></tr>";
-					});
-				} else {
-					document.getElementById("emptyRes").innerText = "Ne podudara se nista sa tom pretragom.";
-					document.getElementById("txtHint").innerText = "";
-				}
-				
-
-            }
-        }
-        xmlhttp.open("GET", "php/productGetter.php?searchFor=" + str, true);
-        xmlhttp.send();
+		$.get("php/productGetter.php?searchFor=" + str, function(data) {
+			data = data.split("||");
+			//removes the last element, which is "||"
+			data.pop();
+			
+			if(data.length != 0) {
+				$("#emptyRes").html("");
+				$("#txtHint").html("<tr><th>RESULT</th><th>DETAILS</th><th>UPDATE</th><th>DELETE</th></tr>");
+				data.forEach(item => {
+					$("#txtHint").html( $("#txtHint").html() + "<tr><td>" + item + "<td><a href='#'>DETAILS</a></td><td><a href='#'>UPDATE</a></td><td><a href='#'>DELETE</a></td></tr>");
+				});
+			} else {
+				$("#emptyRes").html("Ne podudara se nista sa tom pretragom.");
+				$("#txtHint").html("");
+			}
+		});
     }
 }
