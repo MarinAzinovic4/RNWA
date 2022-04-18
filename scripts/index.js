18a7456b973c021b9ee5de2d0bd159186d0d010c
Tin Tadić <tin.tadic46@gmail.com>
2022-04-18 10:01:42 +0200

    DZ 2.3

--- scripts/index.js
+++ scripts/index.js
@@ -3,21 +3,24 @@ function showHint(str) {
         $("#txtHint").html("");
         return;
     } else {
-		$.get("php/productGetter.php?searchFor=" + str, function(data) {
-			data = data.split("||");
-			//removes the last element, which is "||"
-			data.pop();
-			
-			if(data.length != 0) {
-				$("#emptyRes").html("");
-				$("#txtHint").html("<tr><th>RESULT</th><th>DETAILS</th><th>UPDATE</th><th>DELETE</th></tr>");
-				data.forEach(item => {
-					$("#txtHint").html( $("#txtHint").html() + "<tr><td>" + item + "<td><a href='#'>DETAILS</a></td><td><a href='#'>UPDATE</a></td><td><a href='#'>DELETE</a></td></tr>");
-				});
-			} else {
-				$("#emptyRes").html("Ne podudara se nista sa tom pretragom.");
-				$("#txtHint").html("");
+		axios.get("php/productGetter.php?searchFor=" + str)
+			.then((res) => {
+				data = res.data;
+				data = data.split("||");
+				// removes the last element, which is "||"
+				data.pop();
+				
+				if(data.length != 0) {
+					$("#emptyRes").html("");
+					$("#txtHint").html("<tr><th>RESULT</th><th>DETAILS</th><th>UPDATE</th><th>DELETE</th></tr>");
+					data.forEach(item => {
+						$("#txtHint").html( $("#txtHint").html() + "<tr><td>" + item + "<td><a href='#'>DETAILS</a></td><td><a href='#'>UPDATE</a></td><td><a href='#'>DELETE</a></td></tr>");
+					});
+				} else {
+					$("#emptyRes").html("Ne podudara se nista sa tom pretragom.");
+					$("#txtHint").html("");
+				}
 			}
-		});
+		);
     }
 }
