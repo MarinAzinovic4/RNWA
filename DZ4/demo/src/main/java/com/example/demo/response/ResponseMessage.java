15cdfbdee2ea37b579061ed7422fc7f0f0513703
Tin Tadić <tin.tadic46@gmail.com>
2022-05-07 18:34:19 +0200

    set up spring boot backend

---
+++ DZ4/demo/src/main/java/com/example/demo/response/ResponseMessage.java
@@ -0,0 +1,44 @@
+package com.example.demo.response;
+
+import java.util.Date;
+import java.util.List;
+
+import com.example.demo.model.Department;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseMessage {
+
+    private HttpStatus httpStatus;
+
+    private String message;
+
+    private Date timestamp;
+
+    @JsonInclude(Include.NON_NULL)
+    private List<Department> departments;
+    
+    public ResponseMessage(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+        this.timestamp = new Date();
+    }
+
+    public ResponseMessage(HttpStatus httpStatus, String message, List<Department> departments) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+        this.departments = departments;
+        this.timestamp = new Date();
+    }
+}
